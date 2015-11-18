1c5d14144c2b5bdf9ca26237355b77f7b83469f3
Kanstantsin Makarau <Kanstantsin_Makarau@epam.com>
2015-11-18 12:40:50 +0300

    fix nullPointer by setting implementations of Radio and Alarm

--- main/src/main/java/multipleinheritance/MainRadioAlarm.java
+++ main/src/main/java/multipleinheritance/MainRadioAlarm.java
@@ -6,6 +6,8 @@ package multipleinheritance;
 public class MainRadioAlarm {
     public static void main(String[] args){
         RadioAlarm radioAlarm = new RadioAlarm();
+        radioAlarm.setAlarm(new AlarmImpl());
+        radioAlarm.setRadio(new RadioImpl());
         radioAlarm.setAlarmTime();
         radioAlarm.setChannel();
         radioAlarm.setVolume();
