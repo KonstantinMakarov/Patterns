ce465ac0dbe43dda5cc27e1a3db18ddf751f432d
Kanstantsin Makarau <Kanstantsin_Makarau@epam.com>
2015-11-23 11:28:42 +0300

    extend IRobot battery status

--- main/src/main/java/irobot/creationbyfactory/entity/IRobot.java
+++ main/src/main/java/irobot/creationbyfactory/entity/IRobot.java
@@ -4,21 +4,26 @@ import irobot.creationbyfactory.ObjectFactory;
 import irobot.creationbyfactory.entity.cleaner.Cleaner;
 import irobot.creationbyfactory.entity.speaker.Speaker;
 
+import java.util.Random;
+
 /**
  * Created by Kanstantsin_Makarau on 19-Nov-15.
  */
 public class IRobot {
     private Cleaner cleaner;
     private Speaker speaker;
+    private int batteryPercent;
 
     public IRobot() throws Exception {
         speaker = ObjectFactory.getInstance().create(Speaker.class);
         cleaner = ObjectFactory.getInstance().create(Cleaner.class);
+        batteryPercent = 6 + new Random().nextInt(10);
     }
 
     public void cleanRoom() throws Exception {
         speaker.speak("I've started");
         cleaner.cleanRoom();
+        speaker.speak("My battery percent = "+ batteryPercent + "%");
         speaker.speak("I've finished");
     }
 
