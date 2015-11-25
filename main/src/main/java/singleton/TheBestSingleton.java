c424154960d771931ee10fcf8d0520be2968712e
Kanstantsin Makarau <Kanstantsin_Makarau@epam.com>
2015-11-25 14:00:32 +0300

    add comments to Singleton implementation

--- main/src/main/java/singleton/TheBestSingleton.java
+++ main/src/main/java/singleton/TheBestSingleton.java
@@ -4,15 +4,15 @@ package singleton;
  * Created by Kanstantsin_Makarau on 15-Sep-15.
  */
 public class TheBestSingleton {
-    private static volatile TheBestSingleton theBestSingleton;
+    private static volatile TheBestSingleton theBestSingleton;  //mark as volatile
 
     private TheBestSingleton(){
     }
 
     public static TheBestSingleton getInstance(){
         if(theBestSingleton == null){
-            synchronized (TheBestSingleton.class){  //use double check
-                if(theBestSingleton == null) {
+            synchronized (TheBestSingleton.class){  //synchronize when instance is null
+                if(theBestSingleton == null) {  //use double check
                     return theBestSingleton = new TheBestSingleton();
                 }
             }
